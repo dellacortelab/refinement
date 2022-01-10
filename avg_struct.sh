525d96be103c234f50ece818f05ed9a3ae05a00a
cmorri97 <cmorri97@byu.edu>
2022-01-10 10:26:15 -0700

    fix help text and scwrl script

--- avg_struct.sh
+++ avg_struct.sh
@@ -1,18 +1,70 @@
-traj=$1
-structure=$2
+print_help () {
+    echo -e "\n This script creates an averaged structure of a trajectory, then runs SCWRL4 to optimize sidechains and a restrained energy minimization on the averaged structure.\n"
+    echo "[-h]: print this help text and exit."
+    echo "Required Arguments: "
+    echo "[-t TRAJ_PATH]: path to trajectory to create an averaged structure out of."
+    echo "[-s STRUCT_PATH]: path to structure PDB file for trajectory."
+    echo "[--scwrl4 SCWRL_PATH]: path to SCWRL4 executable."
+    echo -e "\nSpecial Dependencies: Gromacs, SCWRL4, OpenMM\n"
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    print_help
+fi
+
+has_traj=false
+has_struct=false
+has_scwrl=false
+
+IFS=' ' read -r -a allArgs <<< "$@"
+for ((idx=0;idx<$#; idx++)); do
+    if [ ${allArgs[${idx}]} == "-h" ]; then
+        print_help
+    elif [ ${allArgs[${idx}]} == "-t" ]; then
+        traj=${allArgs[$(($idx+1))]}
+        has_traj=true
+    elif [ ${allArgs[${idx}]} == "-s" ]; then
+        structure=${allArgs[$(($idx+1))]}
+        has_struct=true
+    elif [ ${allArgs[${idx}]} == "--scwrl4" ]; then
+        scwrl_path=${allArgs[$(($idx+1))]}
+        has_scwrl=true
+    fi
+done
+
+if [ "$has_traj" = false ]; then
+    echo "Missing required argument -t"
+    exit
+elif [ "$has_struct" = false ]; then
+    echo "Missing required argument -s"
+    exit
+elif [ "$has_scwrl" = false ]; then
+    echo "Missing required argument --scwrl4"
+    exit
+fi
+
 base=$(basename $traj .xtc)
 dir=$(dirname $traj)
 
-gmx rmsf -f $file -s $structure -ox ${dir}/${base}_avg.pdb<<EOF
+gmx rmsf -f $traj -s $structure -ox ${dir}/${base}_avg.pdb<<EOF
 0
 EOF
 
-script_path=$(dirname "$0")
+if [ $? -ne 0 ]; then
+    echo 'gmx rmsf command failed to generate an averaged structure'
+    exit
+fi
 
-scwrl_path=$3
+script_path=$(dirname "$0")
 
 #run scwrl
 python $script_path/run_scwrl.py -s ${dir}/${base}_avg.pdb --scwrl4 $scwrl_path
+
+if [ $? -ne 0 ]; then
+    echo 'Failed to run scwrl successfully'
+    exit
+fi
 
 gmx pdb2gmx -f ${dir}/${base}_avg_scwrl.pdb -o ${dir}/${base}_avg_scwrl_prep.pdb -ignh<<EOF
 6
@@ -20,3 +72,13 @@ gmx pdb2gmx -f ${dir}/${base}_avg_scwrl.pdb -o ${dir}/${base}_avg_scwrl_prep.pdb
 EOF
 
 python $script_path/finalEnergyMinim.py ${dir}/${base}_avg_scwrl_prep.pdb
+
+if [ $? -ne 0 ]; then
+    echo 'Failed to energy minimize final structure'
+    exit
+fi
+
+cp ${dir}/${base}_avg_scwrl_prep_minim.pdb ${dir}/${base}_final.pdb
+if [ $? -eq 0 ]; then
+    echo "Final refined structure saved to ${dir}/${base}_final.pdb"
+fi
--- run_scwrl.py
+++ run_scwrl.py
@@ -1,9 +1,7 @@
 import argparse
-from Bio.Data.IUPACData import protein_letters_3to1
-from Bio.SeqUtils import seq1
-from Bio.PDB import *
 import os
 import subprocess
+import random
 
 parser = argparse.ArgumentParser(description='Script to run SCWRL4 on a structure averaged with gmx rmsf')
 parser.add_argument('-s',help='Path to PDB structure to run SCWRL4 on',required=True,dest='struct')
@@ -15,10 +13,8 @@ scwrl_path = args.scwrl4
 
 #Check if first Os are O and OXT, if not, change them. 
 #Run scwrl
-#Change output O's to OC1 and OC2
 fileChanged = False
 #iterate through all lines to change terminal atoms on all chains
-
 with open(struct_path,'r') as f:
     fileLines = f.readlines()
 
@@ -30,24 +26,13 @@ for i,line in enumerate(fileLines):
         fileLines[i] = fileLines[i].replace("OC2","OXT") 
         fileChanged = True
 
-scwrlFile = os.path.join(dir,"temp_scwrl.pdb")
+random_num = random.randint(1,10000)
+scwrlFile = os.path.join(dir,f"temp_scwrl_{random_num}.pdb")
 with open(scwrlFile,"w") as f:
     f.writelines(fileLines)
-parser = PDBParser()
-struct = parser.get_structure('struct',scwrlFile)
-
-res = struct.get_residues()
-allThreeLetter = ''
-for residue in res:
-    allThreeLetter = allThreeLetter + residue.get_resname()
-fullSeq = seq1(allThreeLetter).lower()
-seqFile = os.path.join(dir,"seq.txt")
-with open(seqFile,"w") as f:
-    f.write(fullSeq)
 basename = os.path.basename(struct_path)
 newName = os.path.join(dir,basename[:basename.rfind(".")] + "_scwrl.pdb")
-#testing scwrl with no input sequence file
-scwrlCommand = f"{scwrl_path} -i {scwrlFile} -o {newName} -s {seqFile} -t" 
+scwrlCommand = f"{scwrl_path} -i {scwrlFile} -o {newName}"
 print(f"Scwrl Command: {scwrlCommand}")
 subprocess.run(scwrlCommand.split())
 os.remove(scwrlFile)
